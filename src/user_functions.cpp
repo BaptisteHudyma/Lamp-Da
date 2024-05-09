7f1d9286ec3c47a333ed953c4428cfb2195585c0
BaptHdm <bapt.hudyma95650@gmail.com>
2024-05-09 11:27:34 +0200

    Remove indexable led code

--- src/user_functions.cpp
+++ src/user_functions.cpp
@@ -1,3 +1,44 @@
 #include "user_functions.h"
 
-namespace user {}
+#include "system/behavior.h"
+#include "system/physical/led_power.h"
+
+namespace user {
+
+void power_on_sequence() { ledpower::write_brightness(BRIGHTNESS); }
+
+void power_off_sequence() {}
+
+void brightness_update(const uint8_t brightness) {
+  ledpower::write_brightness(brightness);
+}
+
+void write_parameters() {}
+
+void read_parameters() {}
+
+void button_clicked(const uint8_t clicks) {
+  switch (clicks) {
+    case 0:
+    case 1:
+      break;
+
+    case 2:
+      update_brightness(255, true);
+      break;
+
+    default:
+      // nothing
+      break;
+  }
+}
+
+void button_hold(const uint8_t clicks, const uint32_t holdDuration) {}
+
+void loop() {}
+
+bool should_spawn_thread() { return false; }
+
+void user_thread() {}
+
+}  // namespace user
