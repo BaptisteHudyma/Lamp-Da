c9aad0d2a739931ef0cd8af65b15c6b14d06f5ee
BaptHdm <bapt.hudyma95650@gmail.com>
2024-05-09 11:27:34 +0200

    Remove indexable led code

--- src/user_constants.h
+++ src/user_constants.h
@@ -6,8 +6,8 @@
 const String SOFTWARE_VERSION = "0.1";  // Update when the soft changes version
 
 // parameters of the led strip used
-constexpr float consWattByMeter = 0;  // power consumption (in Watt/meters)
-constexpr float inputVoltage_V = 12;  // voltage (volts)
-constexpr float ledStripLenght_mm = 0;
+constexpr float consWattByMeter = 12;  // power consumption (in Watt/meters)
+constexpr float inputVoltage_V = 12;   // voltage (volts)
+constexpr float ledStripLenght_mm = 91.0 * 25.0;  // 91 sections of 25 mm
 
 #endif
