ff03dd283a0e4364167b45354d2fd49fc3e8a217
BaptHdm <bapt.hudyma95650@gmail.com>
2024-05-09 13:53:01 +0200

    add back color functions

--- src/system/physical/IMU.h
+++ src/system/physical/IMU.h
@@ -1,6 +1,8 @@
 #ifndef IMU_H
 #define IMU_H
 
+#include "../colors/animations.h"
+
 /// Contains the handling of the gyroscope and accelerometer, and some
 /// associated animations
 namespace imu {
@@ -12,6 +14,9 @@ extern void disable();
 
 // disable imu if last use is old
 extern void disable_after_non_use();
+
+void gravity_fluid(const uint8_t fade, const Color& color, LedStrip& strip,
+                   const bool isReset);
 
 }  // namespace imu
 
