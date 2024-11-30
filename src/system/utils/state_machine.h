995f28b45eabb81027939c489f4e5069ae5b60be
BaptHdm <bapt.hudyma95650@gmail.com>
2024-11-30 23:36:51 +0100

    add state machine class

---
+++ src/system/utils/state_machine.h
@@ -0,0 +1,123 @@
+#pragma once
+
+/**
+ * Define a generic state machina logic
+ */
+
+template<typename State> class StateMachine
+{
+public:
+  StateMachine(const State s) : current(s), lastState(s), afterTimeoutState(s), timeout_ms(0) {}
+
+  /**
+   * \brief timeout check, and such, call it often
+   */
+  void run()
+  {
+    // there is a timeout set for this state
+    if (isTimeoutSet)
+    {
+      // timeout reached
+      if (millis() >= timeout_ms)
+      {
+        const bool hasChanged = set_state(afterTimeoutState);
+        // should never have a state timeout go to itself
+        // TODO? assert(hasChanged);
+      }
+    }
+  }
+
+  /**
+   * \brief Set the machine state.
+   * \param[in] s The new state to set
+   * \return True if the state was updated
+   */
+  bool set_state(const State s)
+  {
+    if (s == current)
+    {
+      // ignore a set state with same value
+      return false;
+    }
+    // update the state
+    lastState = current;
+    current = s;
+    // reset the timeout time
+    isTimeoutSet = false;
+    timeout_ms = 0;
+
+    didStateJustChanged = true;
+    return true;
+  }
+
+  /**
+   * \brief set the new current state, with a timeout
+   * \param[in] s the new state
+   * \param[in] timeout The time out delay after which the state will switch automatically to \ref stateOnTimeout
+   * \param[in] stateOnTimeout
+   * \return true is the state changed
+   */
+  bool set_state(const State s, const uint32_t timeout, const State stateOnTimeout)
+  {
+    // set the new state (only if after timeout is not the same)
+    if (set_state(s) and s != stateOnTimeout)
+    {
+      isTimeoutSet = true;
+      // set after timeout state & timeout
+      update_timeout(timeout);
+      afterTimeoutState = stateOnTimeout;
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * \brief Update the current state timeout if a timeout is active
+   */
+  void update_timeout(const uint32_t timeout)
+  {
+    if (isTimeoutSet)
+    {
+      // potential clock overflow, be careful
+      timeout_ms = millis() + timeout;
+    }
+  }
+
+  bool state_just_changed()
+  {
+    const bool temp = didStateJustChanged;
+    didStateJustChanged = false;
+    return temp;
+  }
+
+  /// Return the actual state
+  State get_state() const { return current; }
+
+  /// Return the state before this one
+  State get_last_state() const { return lastState; }
+
+  /// use this in a state with a timeout set to skip to the next state directly
+  void skip_timeout()
+  {
+    if (isTimeoutSet)
+    {
+      set_state(afterTimeoutState);
+    }
+  }
+
+private:
+  /// The current state the algorithm is in
+  State current;
+  /// keep a reference to the last state
+  State lastState;
+
+  /// prevent some errors with clock value wrap
+  bool isTimeoutSet;
+  /// the time this state should timeout and go to /ref afterTimeoutState
+  uint32_t timeout_ms;
+  // The state to go after a timout
+  State afterTimeoutState;
+
+  /// indicates that the state just changed, reseted on read
+  bool didStateJustChanged;
+};
