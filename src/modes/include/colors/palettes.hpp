e02f3a1218c2a940fe67435cc0be6bdad9ccdf85
BaptHdm <bapt.hudyma95650@gmail.com>
2025-09-26 11:25:56 +0200

    Update papi palette

--- src/modes/include/colors/palettes.hpp
+++ src/modes/include/colors/palettes.hpp
@@ -424,22 +424,24 @@ static constexpr PaletteTy PaletteAuroraColors = {0x000000,
                                                   0xff3300,
                                                   0xff0000};
 
-static constexpr PaletteTy PalettePapiColors = {0x0000FF,
-                                                0x0F9DE8,
-                                                0x2BFAFA,
-                                                0x791CF8,
-                                                0x9683EC,
-                                                0xFF0000,
-                                                0xFFFF00,
-                                                0xFF7F00,
-                                                0xE73E05,
-                                                0xFF00FF,
-                                                0x00FF00,
-                                                0x87E990,
-                                                0x54F98D,
-                                                0x660099,
-                                                0xFF007F,
-                                                0xFFFFFF};
+static constexpr PaletteTy PalettePapiColors = {
+        0x0000FF, // deep blue
+        0x0F9DE8, // cerulien blue
+        0x2BFAFA, // cyan
+        0x791CF8, // indigo
+        0x9683EC, // lavander
+        0xFF0000, // red
+        0xFFFF00, // yellow
+        0xFF7F00, // orange
+        0xE73E05, // corail
+        0xFF00FF, // magenta
+        0x00FF00, // green
+        0x87E990, // jade
+        0x058E8E, // teal
+        0x660099, // violet
+        0xFF007F, // pink
+        0xFFFFFF  // white
+};
 
 /// \private return a palette from two colors
 template<uint32_t startColor, uint32_t endColor> static constexpr PaletteTy _gradient_palette_from_color()
