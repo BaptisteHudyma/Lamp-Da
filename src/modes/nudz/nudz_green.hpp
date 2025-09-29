7673b3e8f8d9a160c2998dc4bf548cd1a7e46745
Denis Rivière <denis.riviere.gm@gmail.com>
2025-09-29 19:09:04 +0200

    variable speed

--- src/modes/nudz/nudz_green.hpp
+++ src/modes/nudz/nudz_green.hpp
@@ -1,22 +1,59 @@
 
-#include "heineken_image.hpp"
 
 namespace modes::nudz
 {
 
   struct NudzGreenMode : public BasicMode {
 
-    static void loop(auto& ctx) {
-//       ctx.lamp.fill(colors::Green);
-      // ctx.lamp.setBrightness(ctx.lamp.tick % ctx.lamp.maxBrightness);
-//       ctx.lamp.setPixelColorXY(1, 1, colors::Red);
-      unsigned decal = unsigned(ctx.lamp.tick / 5);
-      unsigned w = 25, h = 22;
-      for(unsigned y=0; y<h; ++y)
-        for(unsigned x=0; x<w; ++x)
-          ctx.lamp.setPixelColorXY((x + decal) % w, y, heineken_image[y * w + x]);
+    struct StateTy
+    {
+      uint32_t decal;
+      uint32_t last_tick;
+    };
+
+#include "heineken_image.hpp"
+
+    static void on_enter_mode(auto& ctx)
+    {
+      // reset stateful events
+      ctx.state.decal = 0;
+      ctx.state.last_tick = 0;
     }
 
+    static void loop(auto& ctx) {
+      // ctx.lamp.fill(colors::Green);
+      // ctx.lamp.setBrightness(ctx.lamp.tick % ctx.lamp.maxBrightness);
+
+      float speed = (float(ctx.get_active_custom_ramp()) - 128) / 500.f;
+      // ease stop at 0
+      if(speed < -0.01)
+        speed += 0.01;
+      else if(speed > 0.01)
+        speed -= 0.01;
+      else
+        speed = 0.f;
+
+      // decal needs a state to keep continuous while speed is changed
+      uint32_t decal = ctx.state.decal
+        + unsigned((ctx.lamp.tick - ctx.state.last_tick) * speed);
+      if(decal != ctx.state.decal)
+      {
+        ctx.state.decal = decal;
+        ctx.state.last_tick = ctx.lamp.tick;
+      }
+
+      uint16_t imWidth = 25;
+      uint16_t imHeight = 22;
+      uint32_t w = std::min(uint32_t(ctx.lamp.maxWidth), uint32_t(imWidth));
+      uint32_t h = std::min(uint32_t(ctx.lamp.maxHeight), uint32_t(imHeight));
+      for(uint32_t y=0; y<h; ++y)
+        for(uint32_t x=0; x<w; ++x)
+          ctx.lamp.setPixelColorXY(x, y,
+                                   heineken_image[y * imWidth
+                                                  + (x + decal) % imWidth]);
+    }
+
+    static constexpr bool hasCustomRamp = true;
   };
 
 }
