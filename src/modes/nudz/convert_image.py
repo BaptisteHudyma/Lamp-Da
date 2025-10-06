92fef3eee86fb21ebcc463fe5181e9770e3c41b3
Denis Rivière <denis.riviere.gm@gmail.com>
2025-10-06 18:11:15 +0200

    allow and use indexed colormap images to save memory

--- src/modes/nudz/convert_image.py
+++ src/modes/nudz/convert_image.py
@@ -20,23 +20,86 @@ image = Image.open(in_img)
 btes = image.tobytes()
 sz = (image.width, image.height)
 
+cmap = {}
+i = 0
+cancel = False
+for y in range(sz[1]):
+    for x in range(sz[0]):
+        if image.mode == 'RGBA':
+            r, g, b = btes[i: i+3]
+            i += 4
+        else:
+            r, g, b = btes[i: i+3]
+            i += 3
+        rgb = (r << 16) | (g << 8) | b
+        if rgb not in cmap:
+            if len(cmap) == 256 \
+                    or (len(cmap) * 4 + len(btes) // 4 >= len(btes)):
+                # colormap compression is useless
+                cmap = {}
+                cancel = True
+                break
+            # print(x, y, ':', hex(rgb), ':', len(cmap))
+            cmap[rgb] = len(cmap)
+    if cancel:
+        break
+
+print('cmap:', len(cmap), 'comp:', len(btes), '->',
+      len(cmap) * 4 + len(btes) // 4)
+if cmap:
+    cmap_i = {v: k for k, v in cmap.items()}
+    # print(cmap)
+    # print(cmap_i)
+
 with open(out_src, 'w') as of:
 
     print(f'''    struct {out_cls} {{
       static constexpr uint16_t width = {image.width};
-      static constexpr uint16_t height = {image.height};
-      static constexpr uint32_t rgbData[] = {{ ''', file=of)
-    i = 0
-    for y in range(sz[1]):
-        for x in range(sz[0]):
-            if image.mode == 'RGBA':
-                r, g, b = btes[i: i+3]
-                i += 4
-            else:
-                r, g, b = btes[i: i+3]
-                i += 3
-            end = '\n' if x == sz[0] - 1 else ' '
-            print('0x%02x%02x%02x,' % (r, g, b), file=of, end=end)
+      static constexpr uint16_t height = {image.height};''', file=of)
+    if cmap:
+        print(f'      static constexpr uint32_t colormapSize = {len(cmap)};',
+              file=of)
+        print('      static constexpr uint32_t colormap[] = {\n'
+              '        ', end='', file=of)
+        for i in sorted(cmap_i.keys()):
+            rgb = cmap_i[i]
+            end = '\n' if i % 8 == 7 else ' '
+            print('0x%06x,' % rgb, end=end, file=of)
+        print('\n      };', file=of)
 
-    print('      };\n    };', file=of)
+        print('      static constexpr uint8_t indexData[] = {', file=of)
+        i = 0
+        for y in range(sz[1]):
+            for x in range(sz[0]):
+                if image.mode == 'RGBA':
+                    r, g, b = btes[i: i+3]
+                    i += 4
+                else:
+                    r, g, b = btes[i: i+3]
+                    i += 3
+                rgb = (r << 16) | (g << 8) | b
+                end = '\n' if (x == sz[0] - 1 or x % 16 == 15) else ' '
+                print('0x%02x,' % cmap[rgb], file=of, end=end)
+        print('      };', file=of)
+
+        print('      static constexpr uint32_t rgbData[] = {};', file=of)
+        print('    };', file=of)
+    else:
+        print('''      static constexpr uint32_t colormapSize = 0;
+      static constexpr uint32_t colormap[] = {};
+      static constexpr uint8_t indexData[] = {};
+      static constexpr uint32_t rgbData[] = { ''', file=of)
+        i = 0
+        for y in range(sz[1]):
+            for x in range(sz[0]):
+                if image.mode == 'RGBA':
+                    r, g, b = btes[i: i+3]
+                    i += 4
+                else:
+                    r, g, b = btes[i: i+3]
+                    i += 3
+                end = '\n' if x == sz[0] - 1 else ' '
+                print('0x%02x%02x%02x,' % (r, g, b), file=of, end=end)
+
+        print('      };\n    };', file=of)
 
