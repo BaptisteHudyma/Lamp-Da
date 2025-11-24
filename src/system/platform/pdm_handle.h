0d12f0a0ad01d89bdb3b917079f38ba8a4f39a9d
BaptHdm <bapt.hudyma95650@gmail.com>
2025-11-25 00:14:44 +0100

    Modulate data height with sound level

---
+++ src/system/platform/pdm_handle.h
@@ -0,0 +1,37 @@
+#ifndef PLATFORM_PDM_HANDLE_H
+#define PLATFORM_PDM_HANDLE_H
+
+#include <array>
+#include <stdint.h>
+#include <cstddef>
+
+namespace microphone {
+
+struct PdmData
+{
+  static constexpr uint16_t SAMPLE_SIZE = 512;
+  std::array<int16_t, SAMPLE_SIZE> data;
+
+  uint64_t sampleDuration_us;
+  uint64_t sampleTime_us;
+
+  // number of sound sample in array
+  uint16_t sampleRead = 0;
+
+  bool is_valid() const { return sampleRead > 0; }
+};
+
+namespace _private {
+
+PdmData get();
+
+// start the microphone readings
+bool start();
+// close the microphone readings
+void stop();
+
+} // namespace _private
+
+} // namespace microphone
+
+#endif
