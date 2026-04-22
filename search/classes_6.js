4c1d24e889379f264423572e68e0c94cc056e6c6
BaptisteHudyma <34839281+BaptisteHudyma@users.noreply.github.com>
2026-04-22 15:31:23 +0000

    Deploying to gh-pages from @ BaptisteHudyma/Lamp-Da@a5d52a87139304bed5340ee6bdd0dfce41f5b91b 🚀

--- search/classes_6.js
+++ search/classes_6.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['globalsimstatety_0',['GlobalSimStateTy',['../structsimulator_1_1globals_1_1GlobalSimStateTy.html',1,'simulator::globals']]],
-  ['gravitymode_1',['GravityMode',['../structlampda_1_1modes_1_1default__modes_1_1GravityMode.html',1,'lampda::modes::default_modes']]]
+  ['gravitydissolve_1',['GravityDissolve',['../structlampda_1_1modes_1_1anims_1_1fadeout_1_1GravityDissolve.html',1,'lampda::modes::anims::fadeout']]],
+  ['gravitydissolve_3c_20dissolvebufferid_20_3e_2',['GravityDissolve&lt; dissolveBufferId &gt;',['../structlampda_1_1modes_1_1anims_1_1fadeout_1_1GravityDissolve.html',1,'lampda::modes::anims::fadeout']]],
+  ['gravitymode_3',['GravityMode',['../structlampda_1_1modes_1_1default__modes_1_1GravityMode.html',1,'lampda::modes::default_modes']]]
 ];
--- search/files_6.js
+++ search/files_6.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['fastfourriertransform_2ehpp_0',['fastFourrierTransform.hpp',['../fastFourrierTransform_8hpp.html',1,'']]],
-  ['fft_2eh_1',['fft.h',['../fft_8h.html',1,'']]],
-  ['filesystem_2eh_2',['fileSystem.h',['../fileSystem_8h.html',1,'']]],
-  ['fireplace_2ehpp_3',['fireplace.hpp',['../fireplace_8hpp.html',1,'']]],
-  ['fixed_5fmodes_2ehpp_4',['fixed_modes.hpp',['../fixed__modes_8hpp.html',1,'']]],
-  ['functions_2eh_5',['functions.h',['../functions_8h.html',1,'']]]
+  ['fadeout_2ehpp_0',['fadeout.hpp',['../fadeout_8hpp.html',1,'']]],
+  ['fastfourriertransform_2ehpp_1',['fastFourrierTransform.hpp',['../fastFourrierTransform_8hpp.html',1,'']]],
+  ['fft_2eh_2',['fft.h',['../fft_8h.html',1,'']]],
+  ['filesystem_2eh_3',['fileSystem.h',['../fileSystem_8h.html',1,'']]],
+  ['fireplace_2ehpp_4',['fireplace.hpp',['../fireplace_8hpp.html',1,'']]],
+  ['fixed_5fmodes_2ehpp_5',['fixed_modes.hpp',['../fixed__modes_8hpp.html',1,'']]],
+  ['functions_2eh_6',['functions.h',['../functions_8h.html',1,'']]]
 ];
