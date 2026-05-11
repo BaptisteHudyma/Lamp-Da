b5fb805c2691fb4b1a7ea6341c110b7aa8eb5132
BaptisteHudyma <34839281+BaptisteHudyma@users.noreply.github.com>
2026-05-11 22:48:06 +0000

    Deploying to gh-pages from @ BaptisteHudyma/Lamp-Da@5a48dcd7db4029922e216c0e52e55ac05c3a2ebe 🚀

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,8 +1,6 @@
 var searchData=
 [
   ['hardwareversiont_0',['HardwareVersiont',['../structbq76905_1_1BQ76905_1_1Regt_1_1HardwareVersiont.html',1,'bq76905::BQ76905::Regt']]],
-  ['heinekenimagety_1',['HeinekenImageTy',['../structHeinekenImageTy.html',1,'']]],
-  ['helixxyzty_2',['HelixXYZTy',['../structlampda_1_1modes_1_1HelixXYZTy.html',1,'lampda::modes']]],
-  ['hsv_3',['HSV',['../classlampda_1_1utils_1_1ColorSpace_1_1HSV.html',1,'lampda::utils::ColorSpace']]],
-  ['huit_5fsiximagety_4',['Huit_sixImageTy',['../structHuit__sixImageTy.html',1,'']]]
+  ['helixxyzty_1',['HelixXYZTy',['../structlampda_1_1modes_1_1HelixXYZTy.html',1,'lampda::modes']]],
+  ['hsv_2',['HSV',['../classlampda_1_1utils_1_1ColorSpace_1_1HSV.html',1,'lampda::utils::ColorSpace']]]
 ];
