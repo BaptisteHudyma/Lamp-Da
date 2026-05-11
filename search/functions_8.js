7609bab900dd1c4fdaba6387d32128511d221a5d
BaptisteHudyma <34839281+BaptisteHudyma@users.noreply.github.com>
2026-05-11 18:47:47 +0000

    Deploying to gh-pages from @ BaptisteHudyma/Lamp-Da@bc3207640e4ed1c410947aa78ac2b8f8acfbeedb 🚀

--- search/functions_8.js
+++ search/functions_8.js
@@ -2,12 +2,17 @@ var searchData=
 [
   ['handle_5fall_0',['handle_all',['../namespacelampda_1_1logic_1_1alerts.html#ab6f8e270ad87c32139571d749506da3a',1,'lampda::logic::alerts']]],
   ['handle_5fble_5felk_5fcommand_1',['handle_BLE_ELK_command',['../namespacelampda_1_1logic_1_1inputs__bluetooth.html#a21531d216f31909b1f76a8bc80dda6e4',1,'lampda::logic::inputs_bluetooth']]],
-  ['handle_5fevents_2',['handle_events',['../namespacelampda_1_1physical_1_1button.html#a291cd2c3049fe4022bf299f307f1cfec',1,'lampda::physical::button']]],
-  ['handle_5flowered_5fstate_3',['handle_lowered_state',['../structlampda_1_1logic_1_1alerts_1_1AlertBase.html#ac37bd05f63da077b6a46253957c05891',1,'lampda::logic::alerts::AlertBase']]],
-  ['handle_5fraised_5fstate_4',['handle_raised_state',['../structlampda_1_1logic_1_1alerts_1_1AlertBase.html#a8795df5a0b70f2fb39ad8df454bbb19a',1,'lampda::logic::alerts::AlertBase']]],
-  ['handle_5fstartup_5',['handle_startup',['../namespacelampda_1_1logic_1_1power.html#aa7c15eaf67ab742f30b9f0da3951819e',1,'lampda::logic::power']]],
-  ['handleserialevents_6',['handleSerialEvents',['../namespacelampda_1_1logic_1_1cli.html#aaa39bb8398d46daa043c64a27b9b2ed5',1,'lampda::logic::cli']]],
-  ['hash_7',['hash',['../namespacelampda_1_1utils.html#a7b68e6e95f92e53533b21590af9cc533',1,'lampda::utils']]],
-  ['hasvalue_8',['hasValue',['../structlampda_1_1modes_1_1ContextTy_1_1KeyProxy.html#a8eb4ba1695cdcf3e64608b9b8384461a',1,'lampda::modes::ContextTy::KeyProxy']]],
-  ['hsv_9',['HSV',['../classlampda_1_1utils_1_1ColorSpace_1_1HSV.html#af36f9b737eb062f5c25699a5db778b99',1,'lampda::utils::ColorSpace::HSV::HSV(const COLOR &amp;c)'],['../classlampda_1_1utils_1_1ColorSpace_1_1HSV.html#acbeacbf87488186dabc67221e4ade5d5',1,'lampda::utils::ColorSpace::HSV::HSV(double h, double s, double v)']]]
+  ['handle_5fbrigthness_5fcontrol_2',['handle_brigthness_control',['../default__behavior_8hpp.html#ac3cb07004d14e044997015caca52dabe',1,'lampda::user::default_behaviors::__private_elk']]],
+  ['handle_5felk_5fcommand_3',['handle_elk_command',['../namespacelampda_1_1user.html#a7c166aecd0393c3af7ee267edb78c9f4',1,'lampda::user']]],
+  ['handle_5fevents_4',['handle_events',['../namespacelampda_1_1physical_1_1button.html#a291cd2c3049fe4022bf299f307f1cfec',1,'lampda::physical::button']]],
+  ['handle_5flowered_5fstate_5',['handle_lowered_state',['../structlampda_1_1logic_1_1alerts_1_1AlertBase.html#ac37bd05f63da077b6a46253957c05891',1,'lampda::logic::alerts::AlertBase']]],
+  ['handle_5fon_5foff_5fcommand_6',['handle_on_off_command',['../default__behavior_8hpp.html#a66b9c654d421139d310774fdbfdf58ff',1,'lampda::user::default_behaviors::__private_elk']]],
+  ['handle_5fpattern_5fselect_5fcommand_7',['handle_pattern_select_command',['../indexable__behavior_8hpp.html#aa3e10ebbd30cde5fd9d6ef4fd5272959',1,'lampda::user::__private_elk']]],
+  ['handle_5fraised_5fstate_8',['handle_raised_state',['../structlampda_1_1logic_1_1alerts_1_1AlertBase.html#a8795df5a0b70f2fb39ad8df454bbb19a',1,'lampda::logic::alerts::AlertBase']]],
+  ['handle_5fspeed_5fcommand_9',['handle_speed_command',['../default__behavior_8hpp.html#ae12fc38efea7ec13653af2fb38fd43cd',1,'lampda::user::default_behaviors::__private_elk']]],
+  ['handle_5fstartup_10',['handle_startup',['../namespacelampda_1_1logic_1_1power.html#aa7c15eaf67ab742f30b9f0da3951819e',1,'lampda::logic::power']]],
+  ['handleserialevents_11',['handleSerialEvents',['../namespacelampda_1_1logic_1_1cli.html#aaa39bb8398d46daa043c64a27b9b2ed5',1,'lampda::logic::cli']]],
+  ['hash_12',['hash',['../namespacelampda_1_1utils.html#a7b68e6e95f92e53533b21590af9cc533',1,'lampda::utils']]],
+  ['hasvalue_13',['hasValue',['../structlampda_1_1modes_1_1ContextTy_1_1KeyProxy.html#a8eb4ba1695cdcf3e64608b9b8384461a',1,'lampda::modes::ContextTy::KeyProxy']]],
+  ['hsv_14',['HSV',['../classlampda_1_1utils_1_1ColorSpace_1_1HSV.html#af36f9b737eb062f5c25699a5db778b99',1,'lampda::utils::ColorSpace::HSV::HSV(const COLOR &amp;c)'],['../classlampda_1_1utils_1_1ColorSpace_1_1HSV.html#acbeacbf87488186dabc67221e4ade5d5',1,'lampda::utils::ColorSpace::HSV::HSV(double h, double s, double v)']]]
 ];
