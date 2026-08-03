190bc9c9d4d48d4bd598809df74768d6d69ae5a0
BaptisteHudyma <34839281+BaptisteHudyma@users.noreply.github.com>
2026-08-03 10:10:37 +0000

    Deploying to gh-pages from @ BaptisteHudyma/Lamp-Da@abb9e749d99f94377fe168b3862ebf3731407a2f 🚀

--- search/classes_14.js
+++ search/classes_14.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['uielement_0',['UIElement',['../structlampda_1_1modes_1_1draw_1_1overlay_1_1____private_1_1UIElement.html',1,'lampda::modes::draw::overlay::__private']]],
-  ['usbpddata_1',['UsbPDData',['../structlampda_1_1bsp_1_1powerDelivery_1_1UsbPDData.html',1,'lampda::bsp::powerDelivery']]]
+  ['uartsendrequest_0',['UartSendRequest',['../structlampda_1_1hal_1_1bluetooth_1_1____private_1_1UartSendRequest.html',1,'lampda::hal::bluetooth::__private']]],
+  ['uielement_1',['UIElement',['../structlampda_1_1modes_1_1draw_1_1overlay_1_1____private_1_1UIElement.html',1,'lampda::modes::draw::overlay::__private']]],
+  ['usbpddata_2',['UsbPDData',['../structlampda_1_1bsp_1_1powerDelivery_1_1UsbPDData.html',1,'lampda::bsp::powerDelivery']]]
 ];
--- search/functions_17.js
+++ search/functions_17.js
@@ -2,11 +2,12 @@ var searchData=
 [
   ['wait_5fnotification_0',['wait_notification',['../namespacelampda_1_1hal_1_1threads.html#a0b42a6cc53564c99c86fb117e05ed4c4',1,'lampda::hal::threads']]],
   ['was_5fstarted_5fin_5fbattery_5frecovery_1',['was_started_in_battery_recovery',['../namespacelampda_1_1logic_1_1power.html#a0ba1dc5c51a39955558feffe629cf321',1,'lampda::logic::power']]],
-  ['when_5flast_5fupdate_5fbrightness_2',['when_last_update_brightness',['../namespacelampda_1_1logic_1_1brightness.html#a0ebc1a296fd81cb735a6002fcca3eb9d',1,'lampda::logic::brightness']]],
-  ['wrap_5fangle_3',['wrap_angle',['../namespacelampda.html#a4a451ce223644730414bf41bfe649b3a',1,'lampda']]],
-  ['write_5fparameters_4',['write_parameters',['../structlampda_1_1modes_1_1ContextTy.html#a6fbae47fcd69a85f9951cee40e64e156',1,'lampda::modes::ContextTy::write_parameters()'],['../structlampda_1_1modes_1_1BasicMode.html#a3934e147a242ba57b99a8fedb09dd6b3',1,'lampda::modes::BasicMode::write_parameters()'],['../namespacelampda_1_1user.html#acb9086987851371577be7407d39032c7',1,'lampda::user::write_parameters()']]],
-  ['write_5ftemporary_5foutput_5flimits_5',['write_temporary_output_limits',['../namespacelampda_1_1component_1_1outputPower.html#a35e4cd6db54fe40fca592ef9ba6f305c',1,'lampda::component::outputPower']]],
-  ['write_5fto_5ffile_6',['write_to_file',['../namespacelampda_1_1component_1_1fileSystem_1_1system.html#a855576dd03cb73a2b4fbb1c609f9e0ea',1,'lampda::component::fileSystem::system::write_to_file()'],['../namespacelampda_1_1component_1_1fileSystem_1_1user.html#a2a50c273be6144d1b114cbfee59d4435',1,'lampda::component::fileSystem::user::write_to_file()']]],
-  ['write_5fto_5fmemory_7',['write_to_memory',['../namespacelampda_1_1logic_1_1statistics.html#a12735c6e3906a32c41384e0c59ac001a',1,'lampda::logic::statistics']]],
-  ['write_5fvoltage_8',['write_voltage',['../namespacelampda_1_1component_1_1outputPower.html#a315f82010750dbd22794b07b7e41d1a8',1,'lampda::component::outputPower']]]
+  ['was_5fused_2',['was_used',['../namespacelampda_1_1hal_1_1bluetooth.html#a6b71732610ad91c1e0a4509526fe006f',1,'lampda::hal::bluetooth']]],
+  ['when_5flast_5fupdate_5fbrightness_3',['when_last_update_brightness',['../namespacelampda_1_1logic_1_1brightness.html#a0ebc1a296fd81cb735a6002fcca3eb9d',1,'lampda::logic::brightness']]],
+  ['wrap_5fangle_4',['wrap_angle',['../namespacelampda.html#a4a451ce223644730414bf41bfe649b3a',1,'lampda']]],
+  ['write_5fparameters_5',['write_parameters',['../structlampda_1_1modes_1_1ContextTy.html#a6fbae47fcd69a85f9951cee40e64e156',1,'lampda::modes::ContextTy::write_parameters()'],['../structlampda_1_1modes_1_1BasicMode.html#a3934e147a242ba57b99a8fedb09dd6b3',1,'lampda::modes::BasicMode::write_parameters()'],['../namespacelampda_1_1user.html#acb9086987851371577be7407d39032c7',1,'lampda::user::write_parameters()']]],
+  ['write_5ftemporary_5foutput_5flimits_6',['write_temporary_output_limits',['../namespacelampda_1_1component_1_1outputPower.html#a35e4cd6db54fe40fca592ef9ba6f305c',1,'lampda::component::outputPower']]],
+  ['write_5fto_5ffile_7',['write_to_file',['../namespacelampda_1_1component_1_1fileSystem_1_1system.html#a855576dd03cb73a2b4fbb1c609f9e0ea',1,'lampda::component::fileSystem::system::write_to_file()'],['../namespacelampda_1_1component_1_1fileSystem_1_1user.html#a2a50c273be6144d1b114cbfee59d4435',1,'lampda::component::fileSystem::user::write_to_file()']]],
+  ['write_5fto_5fmemory_8',['write_to_memory',['../namespacelampda_1_1logic_1_1statistics.html#a12735c6e3906a32c41384e0c59ac001a',1,'lampda::logic::statistics']]],
+  ['write_5fvoltage_9',['write_voltage',['../namespacelampda_1_1component_1_1outputPower.html#a315f82010750dbd22794b07b7e41d1a8',1,'lampda::component::outputPower']]]
 ];
