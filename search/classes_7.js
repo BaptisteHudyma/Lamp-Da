34e878899c87202074ae6777148989d7455af178
BaptisteHudyma <34839281+BaptisteHudyma@users.noreply.github.com>
2026-09-21 09:57:39 +0000

    Deploying to gh-pages from @ BaptisteHudyma/Lamp-Da@dbcbd2a994dd26d1568d5c34473fd9018a2c7219 🚀

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['hal_5ffile_0',['HAL_File',['../structlampda_1_1hal_1_1filesystem_1_1HAL__File.html',1,'lampda::hal::filesystem']]],
-  ['hardwareversiont_1',['HardwareVersiont',['../structbq76905_1_1BQ76905_1_1Regt_1_1HardwareVersiont.html',1,'bq76905::BQ76905::Regt']]],
-  ['helixxyzty_2',['HelixXYZTy',['../structlampda_1_1modes_1_1HelixXYZTy.html',1,'lampda::modes']]],
-  ['hsv_3',['HSV',['../classlampda_1_1utils_1_1ColorSpace_1_1HSV.html',1,'lampda::utils::ColorSpace']]]
+  ['hal_5fmutex_1',['hal_mutex',['../structlampda_1_1hal_1_1mutex_1_1hal__mutex.html',1,'lampda::hal::mutex']]],
+  ['hardwareversiont_2',['HardwareVersiont',['../structbq76905_1_1BQ76905_1_1Regt_1_1HardwareVersiont.html',1,'bq76905::BQ76905::Regt']]],
+  ['helixxyzty_3',['HelixXYZTy',['../structlampda_1_1modes_1_1HelixXYZTy.html',1,'lampda::modes']]],
+  ['hsv_4',['HSV',['../classlampda_1_1utils_1_1ColorSpace_1_1HSV.html',1,'lampda::utils::ColorSpace']]]
 ];
