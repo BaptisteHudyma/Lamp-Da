4da70e90561e3b59836331a7d1510283f5e1d10a
BaptHdm <bapt.hudyma95650@gmail.com>
2026-09-22 18:47:58 +0200

    Increase version to 1.9

--- src/system/utils/constants.h
+++ src/system/utils/constants.h
@@ -42,7 +42,7 @@ namespace lampda {
 static constexpr uint8_t BASE_SOFTWARE_VERSION_MAJOR = 1;
 /// Base minor software version, common to all systems.
 /// increment for every  release.
-static constexpr uint8_t BASE_SOFTWARE_VERSION_MINOR = 8;
+static constexpr uint8_t BASE_SOFTWARE_VERSION_MINOR = 9;
 
 using byte = uint8_t;
 
